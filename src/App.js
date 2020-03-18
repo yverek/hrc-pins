3cafcfe599dd6def80fd8d9209d1c70aca0cefb1
Mauro Dattilo <mau.dattilo@gmail.com>
2020-03-18 15:07:48 +0100

    Fix Header component using react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 
+import { Container } from 'react-bootstrap'
+
 import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="container">
+        <Container>
           <Header />
-        </div>
+
+        </Container>
       </div>
     )
   }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import Image from 'react-bootstrap/Image'
 
 import logoHRC from '../assets/logo72.png'
 
 class Header extends Component {
   render() {
     return (
-      <div class="py-5 text-center">
-        <img class="d-block mx-auto mb-4" src={logoHRC} alt="HRC Logo" width="72" height="72" />
+      <div className="py-5 text-center">
+        <Image className="d-block mx-auto mb-4" src={logoHRC} alt="HRC Logo" />
         <h2>HRC Pins Collection</h2>
-        <p class="lead">A simple app to handle my Hard Rock Café pins</p>
+        <p className="lead">A simple app to handle my Hard Rock Café pins</p>
       </div>
     )
   }
