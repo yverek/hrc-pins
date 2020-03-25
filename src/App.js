c7cfc8e21511dfa8118c9ecd6a495450e0351d4d
Mauro Dattilo <mau.dattilo@gmail.com>
2020-03-25 15:15:04 +0100

    Change App component from class to function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Header } from './components/Header'
 import { Navbar } from './components/Navbar'
 import { Board } from './components/Board'
@@ -6,62 +6,43 @@ import { Container, Row, Col } from 'react-bootstrap'
 import './index.css'
 
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    // thx to this answer https://stackoverflow.com/questions/2236747/what-is-the-use-of-the-javascript-bind-method
-    // this.handleContinentChange = this.handleContinentChange.bind(this)
-    // this.handleSearch = this.handleSearch.bind(this)
-    this.state = {
-      selectedContinent: "worldwide",
-      search: ""
-    }
+export const App = () => {
+  const [selectedContinent, setSelectedContinent] = useState('worldwide')
+  const [search, setSearch] = useState('')
+
+  const handleContinentChange = (event) => {
+    setSelectedContinent(event.target.id)
+    setSearch('')
   }
 
-  handleContinentChange = (event) => {
-    this.setState({
-      selectedContinent: event.target.id,
-      search: ""
-    });
+  const handleSearch = (event) => {
+    setSelectedContinent('worldwide')
+    setSearch(event.target.value)
   }
 
-  handleSearch = (event) => {
-    this.setState({
-      // worldwide because Board component shows all pins at every character submitted by user
-      // it's like a live search
-      selectedContinent: "worldwide",
-      search: event.target.value
-    })
-  }
+  return (
+    <div className="App">
+      <Container>
+        <Header />
+        <Row className="justify-content-md-center">
 
-  render() {
-    return (
-      <div className="App">
-        <Container>
-          <Header />
-          { console.log(process.env) }
-          <Row className="justify-content-md-center">
+          <Col md={4} lg={4} className="sidebar sticky-top">
+            <Navbar
+              selectedContinent={selectedContinent}
+              handleContinentChange={handleContinentChange}
+              handleSearch={handleSearch}
+            />
+          </Col>
 
-            <Col md={4} lg={4} className="sidebar sticky-top">
-              <Navbar 
-                selectedContinent={this.state.selectedContinent}
-                handleContinentChange={this.handleContinentChange}
-                handleSearch={this.handleSearch}
-              />
-            </Col>
+          <Col md={8} lg={8} className="pl-5">
+            <Board
+              selectedContinent={selectedContinent}
+              search={search}
+            />
+          </Col>
 
-            <Col md={8} lg={8} className="pl-5">
-              <Board
-                selectedContinent={this.state.selectedContinent}
-                search={this.state.search}
-              />
-            </Col>
-
-          </Row>
-        </Container>
-      </div>
-    )
-  }
+        </Row>
+      </Container>
+    </div>
+  )
 }
-
-export default App
