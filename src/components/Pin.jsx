5711907bd9f440f0a7f3e07626d487f84ca11d14
Mauro Dattilo <mau.dattilo@gmail.com>
2020-03-25 14:41:53 +0100

    Remove comments from Pin component file

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -5,7 +5,7 @@ import { faGlobeAfrica, faFlag, faCity } from '@fortawesome/free-solid-svg-icons
 
 
 export const Pin = (props) => {
-  const [show, handleShow] = useState(0)
+  const [show, handleShow] = useState(false)
 
   const imgSrc = '/pin_pics/' + props.continent_id + '/' + props.country_id + '/' + props.city_id + '.jpg'
 
@@ -57,63 +57,3 @@ export const Pin = (props) => {
     </div>
   )
 }
-
-// This is the class based equivalent
-
-/* 
-class Pin extends Component {
-  constructor(props) {
-    super(props)
-    this.handleShow = this.handleShow.bind(this)
-    this.state = {
-      show: false
-    }
-  }
-
-  handleShow() {
-    this.setState(state => ({
-      show: !state.show
-    }))
-  }
-
-  render() {
-    let imgSrc = '/pin_pics/' + this.props.continent_id + '/' + this.props.country_id + '/' + this.props.city_id + '.jpg'
-    return(
-      <>
-        <Media className="py-2 align-items-center">
-          <Image
-            width={114}
-            height={150}
-            className="align-self-center mr-3"
-            src={imgSrc}
-            alt={this.props.city}
-            onClick={this.handleShow} />
-          <Media.Body>
-            <h5 className="mt-0">Continent: {this.props.continent}</h5>
-            <h5 className="mt-0">Country: {this.props.country}</h5>
-            <h5 className="mt-0">City: {this.props.city}</h5>
-            <p></p>
-          </Media.Body>
-        </Media>
-        <Modal show={this.state.show} onHide={this.handleShow} centered>
-          <Modal.Header>
-            <Modal.Title>{this.props.country} - {this.props.city}</Modal.Title>
-          </Modal.Header>
-        
-          <Modal.Body>
-            <Image
-              className="align-self-center mr-3"
-              src={imgSrc}
-              alt={this.props.city} />
-          </Modal.Body>
-        
-          <Modal.Footer>
-            <Button variant="btn my-btn" onClick={this.handleShow}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    )
-  }
-}
-
-export default Pin */
