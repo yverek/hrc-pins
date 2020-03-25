29c46a85f84380c477a18a424aa65bdca0fe9f32
Mauro Dattilo <mau.dattilo@gmail.com>
2020-03-25 14:43:01 +0100

    Remove comments from Continent component file

--- src/components/Continent.jsx
+++ src/components/Continent.jsx
@@ -18,28 +18,3 @@ export const Continent = (props) => {
     </li>
   )
 }
-
-// This is the class based equivalent
-
-/* 
-class Continent extends Component {
-  render() {
-    return (
-      <li className={"list-group-item " + (!this.props.lastContinent ? 'continent' : '')}>
-        <div className="custom-control custom-radio">
-          <input
-            type="radio"
-            className="custom-control-input"
-            id={this.props.id}
-            name="continents"
-            defaultChecked={this.props.firstContinent}
-            checked={this.props.checked} />
-          <label className="custom-control-label" htmlFor={this.props.id}>{this.props.name}</label>
-          <span className="badge">{this.props.count}</span>
-        </div>
-      </li>
-    )
-  }
-}
-
-export default Continent*/
